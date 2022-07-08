68bcbc34169bcba4a70faf87f17688e483c51aed
nishant4713 <108873242+nishant4713@users.noreply.github.com>
2022-07-08 18:52:16 +0530

    Update and rename .gitignore to Question.java

---
+++ Question.java
@@ -0,0 +1,17 @@
+package Practice_Questions;
+import java.util.*;
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+}
+
